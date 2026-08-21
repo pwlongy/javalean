f90b7e14cde9cb4502f5ae7543278f19a428fb22
pengw <pengw@chinaoly.com>
2026-08-21 18:33:57 +0800

    feat: Array

---
+++ src/com/erjieduan/ArrayTest.java
@@ -0,0 +1,17 @@
+package com.erjieduan;
+
+/**
+ *
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+
+    }
+
+    public static void ArrayMethods() {
+        Integer[] arr = {199, 200, 9999, 10000, 6544545, 6565655, 312659, 99965};
+//      1. toString() 返回数组字符串形式
+        System.out.println(arr.toString());
+//      2. sort 排序
+    }
+}
