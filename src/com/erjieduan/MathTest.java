f43eada8da49df60a54172db867da1dac710f45c
pengw <pengw@chinaoly.com>
2026-08-21 16:53:29 +0800

    feat: math

--- src/com/erjieduan/MathTest.java
+++ src/com/erjieduan/MathTest.java
@@ -5,5 +5,17 @@ package com.erjieduan;
  */
 public class MathTest {
     public static void main(String[] args) {
+//      Math 类中的常量和方法
+        System.out.println(Math.PI); // 圆周率
+        System.out.println(Math.E); // 自然对数的底数
+        System.out.println(Math.sqrt(9)); // 开平方
+        System.out.println(Math.pow(2, 3)); // 2的3次方
+        System.out.println(Math.abs(-10)); // 绝对值
+        System.out.println(Math.ceil(10.1)); // 向上取整
+        System.out.println(Math.floor(10.9)); // 向下取整
+        System.out.println(Math.round(10.5)); // 四舍五入
+        System.out.println(Math.random()); // 随机数
+        System.out.println(Math.max(10, 20)); // 最大值
+        System.out.println(Math.min(10, 20)); // 最小值
     }
 }
